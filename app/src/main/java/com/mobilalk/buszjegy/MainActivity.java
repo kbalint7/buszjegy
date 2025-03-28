68fc722cfdb0ebeb9bd7594f56ffac010838f093
kbalint7 <196464789+kbalint7@users.noreply.github.com>
2025-03-28 19:32:37 +0100

    Initial commit

---
+++ app/src/main/java/com/mobilalk/buszjegy/MainActivity.java
@@ -0,0 +1,49 @@
+package com.mobilalk.buszjegy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    protected static final int SECRET_KEY = 70;
+
+    EditText userNameET;
+    EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        userNameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", jelszó: " + password);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+}
